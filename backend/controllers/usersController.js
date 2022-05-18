55e96033ad0821c7f7a692e749c2dc6f9ad776a8
Kenan Dropić <dropic5@hotmail.com>
2022-05-18 12:55:08 +0200

    delete user

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,10 +1,6 @@
 import User from "../models/User.js";
 import asyncHandler from "express-async-handler";
-import {
-  BadRequestError,
-  NotFoundError,
-  UnAuthenticatedError,
-} from "../utils/errorResponse.js";
+import { BadRequestError, NotFoundError } from "../utils/errorResponse.js";
 
 // @desc    Get all users
 // @route   GET /api/v1/users
@@ -17,4 +13,19 @@ export const getUsers = asyncHandler(async (req, res, next) => {
   }
 
   res.status(200).json({ success: true, users });
+});
+
+// @desc    Delete user
+// @route   DELETE /api/v1/users/:id
+// @access  Private/ADMIN
+export const deleteUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return next(new NotFoundError("User not found"));
+  }
+
+  await user.remove();
+
+  res.status(200).json({ success: true, data: {} });
 });
--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { getUsers } from "../controllers/usersController.js";
+import { deleteUser, getUsers } from "../controllers/usersController.js";
 import { authenticate, authorize } from "../middleware/auth.js";
 
 const router = express.Router();
 
+router.use(authenticate);
+router.use(authorize);
+
 // Admin only
-router.route("/").get(authenticate, authorize, getUsers);
+router.route("/").get(getUsers);
+router.route("/:id").delete(deleteUser);
 
 export default router;
