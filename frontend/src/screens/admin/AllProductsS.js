fd3bdca4f2b2afe6ea1945123c435f204539bbaa
Kenan Dropić <dropic5@hotmail.com>
2022-05-21 14:17:07 +0200

    list products and create

--- frontend/src/screens/admin/AllProductsS.js
+++ frontend/src/screens/admin/AllProductsS.js
@@ -1,7 +1,99 @@
-import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Table, Button, Row, Col } from "react-bootstrap";
+import Loader from "../../components/Loader";
+import Message from "../../components/Message";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  deleteProduct,
+  listProducts,
+} from "../../features/products/productsSlice";
 
 const AllProductsS = () => {
-  return <div>allProductsS</div>;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { products, loading, error, isDeleted } = useSelector(
+    (state) => state.products
+  );
+
+  useEffect(() => {
+    dispatch(listProducts());
+    // eslint-disable-next-line
+  }, [dispatch, isDeleted]);
+
+  const handleDelete = (id) => {
+    if (window.confirm("Da li ste sigurni da želite obrisati proizvod")) {
+      dispatch(deleteProduct(id));
+    }
+  };
+
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message>{error}</Message>
+      ) : (
+        <>
+          <Row className="align-items-center justify-content-between">
+            <Col>
+              <h2>PROIZVODI</h2>
+            </Col>
+            <Col className="d-flex justify-content-end">
+              <Button
+                className="my-3"
+                onClick={() => navigate("/admin/products/create")}
+              >
+                <i className="fas fa-plus" /> Kreirajte proizvod
+              </Button>
+            </Col>
+          </Row>
+          <Row>
+            <Table striped bordered hover responsive className="table-sm">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>IME</th>
+                  <th>CIJENA</th>
+                  <th>KATEGORIJA</th>
+                  <th>BREND</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {products?.length > 0 &&
+                  products?.map((product, idx) => {
+                    return (
+                      <tr key={idx}>
+                        <td>{product._id}</td>
+                        <td>{product.name}</td>
+                        <td>{product.price}</td>
+                        <td>{product.category}</td>
+                        <td>{product.brand}</td>
+                        <td>
+                          <Link to={`${product._id}/edit`}>
+                            <Button variant="light" className="btn-sm">
+                              <i className="fas fa-edit" />
+                            </Button>
+                          </Link>
+                          <Button
+                            variant="danger"
+                            className="btn-sm"
+                            onClick={() => handleDelete(product._id)}
+                          >
+                            <i className="fas fa-trash" />
+                          </Button>
+                        </td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </Table>
+          </Row>
+        </>
+      )}
+    </>
+  );
 };
 
 export default AllProductsS;
--- frontend/src/screens/admin/CreateProductS.js
+++ frontend/src/screens/admin/CreateProductS.js
@@ -1,14 +1,20 @@
-frontend/src/screens/admin/CreateProductS.jsimport { useForm } from "react-hook-form";
-import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Button, Form } from "react-bootstrap";
 import Message from "../../components/Message";
 import Loader from "../../components/Loader";
 import FormContainer from "../../components/FormContainer";
-import { createProduct } from "../../features/products/productsSlice";
+import {
+  createProduct,
+  resetIsStates,
+} from "../../features/products/productsSlice";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const CreateProductS = () => {
+  const [uploading, setUploading] = useState(false);
+  const [imageUrl, setImageUrl] = useState("");
   const navigate = useNavigate();
 
   const dispatch = useDispatch();
@@ -33,15 +39,38 @@ const CreateProductS = () => {
   useEffect(() => {
     if (isCreated) {
       navigate("/admin/products");
+      dispatch(resetIsStates());
     }
     // eslint-disable-next-line
   }, [isCreated]);
+
+  // upload image
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append("image", file);
+    setUploading(true);
+
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      };
+      const { data } = await axios.post("/api/v1/upload", formData, config);
+      setImageUrl(data);
+      setUploading(false);
+    } catch (error) {
+      console.error(error);
+      setUploading(false);
+    }
+  };
 
   const onSubmit = (data) => {
     const copyData = {
       name: data.name,
       price: data.price,
-      image: `/images/${data.image[0].name}`,
+      image: imageUrl,
       brand: data.brand,
       countInStock: data.countInStock,
       category: data.category,
@@ -93,17 +122,20 @@ const CreateProductS = () => {
           </span>
         </Form.Group>
         <Form.Group>
-          <Form.Label>Slike</Form.Label>
+          <Form.Label>Slika</Form.Label>
           <Form.Control
             {...register("image", {
               required: "Polje je obavezno",
             })}
+            onChange={handleImageUpload}
             type="file"
             accept="image/*"
+            name="image"
           />
           <span style={{ color: "red", display: "block" }}>
             {errors.image?.message}
           </span>
+          {uploading && <Loader />}
         </Form.Group>
         <Form.Group>
           <Form.Label>Brand</Form.Label>
