cfee522595eeaa1413c37631b800aa3a150ea103
Kenan Dropić <dropic5@hotmail.com>
2022-05-24 18:32:53 +0200

    reviews frontend

---
+++ frontend/src/components/Reviews.js
@@ -0,0 +1,123 @@
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { Row, Form, Button, ListGroup } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { addReview, getProductReviews } from "../features/reviews/reviewsSlice";
+import Message from "./Message";
+import Rating from "./Rating";
+import { Link } from "react-router-dom";
+import Loader from "./Loader";
+
+const Reviews = ({ productId }) => {
+  const dispatch = useDispatch();
+  const { reviews, error, loading, isAdded } = useSelector(
+    (state) => state.reviews
+  );
+  const { user } = useSelector((state) => state.users);
+  const ratings = Array.from({ length: 5 }, (_, i) => i + 1);
+
+  useEffect(() => {
+    dispatch(getProductReviews(productId));
+  }, [dispatch, productId, isAdded]);
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      rating: 1,
+      comment: "",
+    },
+  });
+
+  const onSubmit = (data) => {
+    dispatch(addReview([productId, data]));
+    reset();
+  };
+
+  return (
+    <>
+      <h2 className="mb-2 p-0">Recenzije</h2>
+      {reviews === null || reviews.length === 0 ? (
+        <Message variant="info">Prozivod nema recenzija</Message>
+      ) : loading ? (
+        <Loader />
+      ) : (
+        <Row>
+          <ListGroup variant="flush">
+            {reviews.map((review) => {
+              return (
+                <ListGroup.Item key={review._id}>
+                  <p>
+                    <strong>{review.name}</strong>
+                  </p>
+                  <Rating
+                    value={review.rating}
+                    text={`${String(reviews?.length)} recenzija`}
+                  />
+                  <p>{review.createdAt.substring(0, 10)}</p>
+                  <p>{review.comment}</p>
+                </ListGroup.Item>
+              );
+            })}
+            <ListGroup.Item>
+              <h2 className="mb-1 p-2">Ostavite recenziju</h2>
+              {error && <Message variant="danger">{error}</Message>}
+              {user !== null ? (
+                <Form
+                  className="my-0 py-0 px-2"
+                  onSubmit={handleSubmit(onSubmit)}
+                >
+                  <Form.Group>
+                    <Form.Label>Ocjena</Form.Label>
+                    <Form.Control as="select" {...register("rating")}>
+                      {ratings.map((rating) => {
+                        return (
+                          <option value={rating} key={rating}>
+                            {rating} -{" "}
+                            {`${
+                              rating === 1
+                                ? "Loše"
+                                : rating === 2
+                                ? "Solidno"
+                                : rating === 3
+                                ? "Dobro"
+                                : rating === 4
+                                ? "Vrlo dobro"
+                                : "Odlično"
+                            }`}
+                          </option>
+                        );
+                      })}
+                    </Form.Control>
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label>Komentar</Form.Label>
+                    <Form.Control
+                      {...register("comment")}
+                      as="textarea"
+                      row="3"
+                    ></Form.Control>
+                  </Form.Group>
+                  <Button type="submit" className="btn mt-3">
+                    Dodajte recenziju
+                  </Button>
+                </Form>
+              ) : (
+                <Message variant="info">
+                  <Link to="/login">Prijavite se</Link> kako bi mogli ostaviti
+                  recenziju
+                </Message>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Row>
+      )}
+      {/* <Row className="mt-3"></Row> */}
+    </>
+  );
+};
+
+export default Reviews;
---
+++ frontend/src/features/reviews/reviewsSlice.js
@@ -0,0 +1,90 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axiosAuth from "../../utils/axiosAuth";
+import { toast } from "react-toastify";
+
+const initialState = {
+  reviews: null,
+  isEdited: false,
+  loading: false,
+  isAdded: false,
+  error: "",
+};
+
+// get product reviews
+export const getProductReviews = createAsyncThunk(
+  "reviews/get",
+  async (id, thunkAPI) => {
+    try {
+      const {
+        data: { reviews },
+      } = await axiosAuth.get(`/products/${id}/reviews`);
+
+      return reviews;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(errorMessage(error));
+    }
+  }
+);
+
+// create review
+export const addReview = createAsyncThunk(
+  "reviews/add",
+  async ([id, reviewInfo], thunkAPI) => {
+    try {
+      const {
+        data: { reviews },
+      } = await axiosAuth.post(
+        `/products/${[id, reviewInfo][0]}/reviews`,
+        [id, reviewInfo][1]
+      );
+      return reviews;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(errorMessage(error));
+    }
+  }
+);
+
+const adminSlice = createSlice({
+  name: "reviews",
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(getProductReviews.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getProductReviews.fulfilled, (state, action) => {
+        state.loading = false;
+        state.reviews = action.payload;
+        state.error = "";
+      })
+      .addCase(getProductReviews.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(addReview.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(addReview.fulfilled, (state, action) => {
+        state.loading = false;
+        state.isAdded = true;
+        state.error = "";
+      })
+      .addCase(addReview.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+// structure error message and return
+const errorMessage = (error) => {
+  const message =
+    (error.response && error.response.data && error.response.data.error) ||
+    error.message ||
+    error.toString();
+
+  return message;
+};
+
+export default adminSlice.reducer;
